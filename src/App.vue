03d14b67d77afa875bbc4f45ef9874df383723e3
Mary Louise Hermosa <jayzces@gmail.com>
2024-04-24 12:04:40 +0200

    Initial work

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="min-h-screen">
+    <div class="container py-10">
+      <div class="grid grid-cols-2 gap-10">
+        <div class="w-full">
+          <h1 class="block text-3xl font-semibold text-white">Sortable Posts List</h1>
+
+          <div class="list mt-6"></div>
+        </div>
+
+        <div class="w-full">hey</div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  @apply relative h-full before:content-[''] before:block before:absolute before:inset-x-0 before:bg-purple-800 before:h-1/3 before:z-[-1];
+}
+
+main::before {
+  clip-path: polygon(0 0, 0 100%, 100% 0);
+}
+</style>
---
+++ src/components/PostItem.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import type { Post } from '@/stores/types/post'
+defineProps<{ post: Post }>()
+</script>
+
+<template>
+  <div class="post py-8 px-4 bg-white shadow-md rounded text-lg hover:shadow-xl transition-shadow">
+    Post {{ post.id }}
+  </div>
+</template>
